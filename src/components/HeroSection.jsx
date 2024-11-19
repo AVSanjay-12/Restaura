da23dcb8ad8272ddacbcca4a1a783555f0853501
Sanjay A V <sanjayav1262@gmail.com>
2024-11-19 16:11:21 +0530

    first commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,22 @@
+import video from "../assets/hero.mp4";
+import logo from "../assets/logo.png";
+import hero from "../assets/hero.jpeg";
+
+const HeroSection = () => {
+  return (
+    <section className="relative flex h-screen items-center justify-center">
+      <div className="absolute inset-0 -z-20 h-full w-full overflow-hidden">
+        <video
+          src={video}
+          className="h-full w-full object-cover"
+          muted
+          autoPlay
+          loop
+          playsInline
+          poster={hero}
+        ></video>
+      </div>
+    </section>
+  );
+};
+export default HeroSection;

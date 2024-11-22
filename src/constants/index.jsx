9ae794e38f2b72ed59d5353eda27351fe9c47baa
Sanjay A V <sanjayav1262@gmail.com>
2024-11-22 07:07:00 +0530

    Update Contact section

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -121,7 +121,7 @@ export const REVIEW = {
 export const CONTACT = [
   { key: "address", value: "Address: 123 Main Street, Paris, France, 345678" },
   { key: "phone", value: "Phone: 123-456-7890" },
-  { key: "email", value: "Email: contact@restaurant.com" },
+  { key: "email", value: "Email: contact@restaura.com" },
 ];
 
 export const SOCIAL_MEDIA_LINKS = [
